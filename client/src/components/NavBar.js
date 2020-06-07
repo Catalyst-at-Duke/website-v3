5081c37a36e16047f09e794fcff6bbab4493fbe7
Samantha Whitt <smw81@duke.edu>
2020-06-07 19:13:01 -0400

    changing colors

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,38 @@
 import React from "react";
 import * as ReactBootstrap from "react-bootstrap";
 import "../styles.css";
+import { colors, fonts } from "../theme.js";
 
 import CatalystLogo from "../images/logoNoTextNoBack.png";
 
 export default class NavBarComponent extends React.Component {
   render() {
     return (
-      <div flex-direction="row">
+      <div flex-direction="row" style={{ backgroundColor: colors.darkGray }}>
         <ReactBootstrap.Navbar fixed="top" sticky="top" expand="lg">
           <ReactBootstrap.Navbar.Brand href="/">
             <img src={CatalystLogo} alt={"logo"} height="50px" width="50px" />
           </ReactBootstrap.Navbar.Brand>
           <ReactBootstrap.Nav class="d-flex justify-content-end">
-            <ReactBootstrap.Nav.Link class="mr-auto p-2" href="/members">
+            <ReactBootstrap.Nav.Link
+              class="mr-auto p-2"
+              href="/members"
+              style={{ color: colors.white }}
+            >
               members
             </ReactBootstrap.Nav.Link>
-            <ReactBootstrap.Nav.Link class="mr-auto p-2" href="/recruitment">
+            <ReactBootstrap.Nav.Link
+              class="mr-auto p-2"
+              href="/recruitment"
+              style={{ color: colors.white }}
+            >
               recruitment
             </ReactBootstrap.Nav.Link>
-            <ReactBootstrap.Nav.Link class="mr-auto p-2" href="/faq">
+            <ReactBootstrap.Nav.Link
+              class="mr-auto p-2"
+              href="/faq"
+              style={{ color: colors.white }}
+            >
               faq
             </ReactBootstrap.Nav.Link>
           </ReactBootstrap.Nav>
--- client/src/home.js
+++ client/src/home.js
@@ -1,44 +1,29 @@
-import React from 'react';
-import Switch from 'react-switch';
-import classNames from 'classnames';
+import React from "react";
+import Switch from "react-switch";
+import classNames from "classnames";
+import { colors, fonts } from "./theme.js";
 // import * as ReactBootstrap from 'react-bootstrap';
-import './styles.css';
+import "./styles.css";
 
-import CatalystLogo from './images/logoNoTextNoBack.png';
+import CatalystLogo from "./images/logoNoTextNoBack.png";
 
 export default class HomeComponent extends React.Component {
   constructor() {
     super();
-    this.state = {
-      checked: true, // y dark, n light
-    };
-  }
-
-  handleChange = () => {
-    this.setState((state) => ({
-      checked: !this.state.checked,
-    }));
   }
 
   render() {
-    const themeClass = this.state.checked ? 'dark-theme' : 'light-theme';
-    const headerClass = classNames('app', themeClass);
-
     return (
-      <div className={headerClass}>
-        <div className='home-body'>
-          <div className='catalyst-logo-text'>
-            <img src={CatalystLogo} alt={'logo'}/>
-            <div className='catalyst-text'>
-              <TextGlitch word='C A T A L Y S T'>
-              </TextGlitch>
+      <div className="app">
+        <div
+          className="home-body"
+          style={{ backgroundColor: colors.darkGray, color: colors.white }}
+        >
+          <div className="catalyst-logo-text">
+            <img src={CatalystLogo} alt={"logo"} />
+            <div className="catalyst-text">
+              <TextGlitch word="C A T A L Y S T"></TextGlitch>
             </div>
-          </div>
-          <div>
-            <ToggleSwitch
-              checked={this.state.checked}
-              handleChange={this.handleChange}
-            />
           </div>
         </div>
       </div>
@@ -46,41 +31,23 @@ export default class HomeComponent extends React.Component {
   }
 }
 
-class ToggleSwitch extends React.Component {
-  render() {
-    return (
-      <div className='example'>
-        <Switch
-          onChange={this.props.handleChange}
-          checked={!this.props.checked}
-          onColor='#83dae8'
-          onHandleColor='#08b5d1'
-          uncheckedIcon={false}
-          checkedIcon={false}
-          className='react-switch'
-        />
-      </div>
-    );
-  }
-}
-
 class TextGlitch extends React.Component {
   render() {
-    const letters = this.props.word.split('');
+    const letters = this.props.word.split("");
 
     return (
-      <div className='glitch-wrapper' role='p'>
-        {
-          letters.map((l) => (
-            l === ' ' ?
-              <span className='glitch-letter'>&nbsp;</span> :
-              (
-                <div className='glitch-wrapper'>
-                  <span className='glitch-letter' data-text={l}>{ l }</span>
-                </div>
-              )
-          ))
-        }
+      <div className="glitch-wrapper" role="p">
+        {letters.map((l) =>
+          l === " " ? (
+            <span className="glitch-letter">&nbsp;</span>
+          ) : (
+            <div className="glitch-wrapper">
+              <span className="glitch-letter" data-text={l}>
+                {l}
+              </span>
+            </div>
+          )
+        )}
       </div>
     );
   }
