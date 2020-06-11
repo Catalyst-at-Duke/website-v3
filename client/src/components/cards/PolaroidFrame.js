61fdf77dff9d04f1f9fea1cd52c87e9d4fac6d6f
Samantha Whitt <smw81@duke.edu>
2020-06-11 00:49:11 -0400

    tape addition

--- client/src/components/cards/PolaroidFrame.js
+++ client/src/components/cards/PolaroidFrame.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { colors, fonts } from "../../styles/theme.js";
 import "./cards.css";
+import Tape from "../../images/tape.png";
 
 export default class PolaroidFrame extends React.Component {
   render() {
     return (
-      <div class="polaroid-outer" style={{ backgroundColor: colors.white }}>
-        <div
-          class="polaroid-inner"
-          style={{ backgroundColor: colors.black }}
-        ></div>
-        <div style={{ fontSize: fonts.size.normal }}>Nathan Ostrowski</div>
-        <div
-          style={{
-            fontSize: fonts.size.normal,
-            fontWeight: fonts.weights.light,
-          }}
-        >
-          President
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div class="tape" style={{ background: `url(${Tape})` }} />
+        <div class="polaroid-outer" style={{ backgroundColor: colors.white }}>
+          <div
+            class="polaroid-inner"
+            style={{ backgroundColor: colors.black }}
+          ></div>
+          <div style={{ fontSize: fonts.size.normal }}>Nathan Ostrowski</div>
+          <div
+            style={{
+              fontSize: fonts.size.normal,
+              fontWeight: fonts.weights.light,
+            }}
+          >
+            President
+          </div>
         </div>
       </div>
     );
