ad475f00dd0a7dd7a30449ba4e09a6f658f69cee
Samantha Whitt <smw81@duke.edu>
2020-06-11 01:16:33 -0400

    fixing positioning

--- client/src/components/cards/PolaroidFrame.js
+++ client/src/components/cards/PolaroidFrame.js
@@ -12,46 +12,49 @@ export default class PolaroidFrame extends React.Component {
   }
   render() {
     return (
-      <div
-        className="polaroid-outer"
-        style={{ backgroundColor: colors.white }}
-        onMouseLeave={() => this.setState({ hovered: false })}
-        onMouseEnter={() => this.setState({ hovered: true })}
-      >
-        <div className="polaroid-inner">
-          <img
+      <div>
+        {/* <div style={{ background: `url(${Tape})` }}></div> */}
+        <div
+          className="polaroid-outer"
+          style={{ backgroundColor: colors.white }}
+          onMouseLeave={() => this.setState({ hovered: false })}
+          onMouseEnter={() => this.setState({ hovered: true })}
+        >
+          <div className="polaroid-inner">
+            <img
+              style={{
+                backgroundColor: colors.black,
+                width: "200px",
+                height: "190px",
+                objectFit: "cover",
+              }}
+              src={this.props.photo}
+            />
+          </div>
+          <div style={{ fontSize: fonts.size.normal }}>{this.props.name}</div>
+          <div
             style={{
-              backgroundColor: colors.black,
-              width: "200px",
-              height: "190px",
-              objectFit: "cover",
+              fontSize: fonts.size.normal,
+              fontWeight: fonts.weights.light,
             }}
-            src={this.props.photo}
-          />
-        </div>
-        <div style={{ fontSize: fonts.size.normal }}>{this.props.name}</div>
-        <div
-          style={{
-            fontSize: fonts.size.normal,
-            fontWeight: fonts.weights.light,
-          }}
-        >
-          {this.props.position}
-        </div>
+          >
+            {this.props.position}
+          </div>
 
-        <div
-          style={{
-            position: "absolute",
-            width: "230px",
-            overflow: "auto",
-            height: "280px",
-            backgroundColor: colors.white,
-            padding: "5px",
-            transition: "opacity .2s",
-            opacity: this.state.hovered ? "95%" : "0%",
-          }}
-        >
-          {this.props.message}
+          <div
+            style={{
+              position: "absolute",
+              width: "230px",
+              overflow: "auto",
+              height: "280px",
+              backgroundColor: colors.white,
+              padding: "5px",
+              transition: "opacity .2s",
+              opacity: this.state.hovered ? "95%" : "0%",
+            }}
+          >
+            {this.props.message}
+          </div>
         </div>
       </div>
     );
