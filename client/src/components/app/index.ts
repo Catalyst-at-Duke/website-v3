ad83b2ec5f12970f30da5b352bd309dc07d95d7f
Samuel Zhang <samzyao@gmail.com>
2020-08-12 03:23:35 -0400

    reorganized home component and test into /app

--- client/src/components/app/index.ts
+++ client/src/components/app/index.ts
@@ -1,4 +1,8 @@
+import About from "./About";
 import Faq from "./Faq";
+import Home from "./home";
 import Members from "./Members";
+import NavBar from "./NavBar";
+import Recruitment from "./Recruitment";
 
-export { Faq, Members };
+export { About, Faq, Home, Members, NavBar, Recruitment };
