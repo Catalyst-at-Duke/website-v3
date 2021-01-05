95c04802616e243a2fde75511005d0ef6bb7800c
2018ayee <adam.yee@gmail.com>
2021-01-05 01:18:04 -0500

    Wrote FAQ page

--- client/src/pages/Faq.js
+++ client/src/pages/Faq.js
@@ -3,13 +3,21 @@ import Typist from "react-typist";
 import TypistLoop from "react-typist-loop";
 
 import { colors } from "styles/theme.js";
+import { fonts } from "styles/theme.js";
 import "styles/styles.css";
 
 export default class FaqComponent extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="body" style={{ backgroundColor: colors.yellow }}>
+        <div
+          className="body"
+          style={{
+            backgroundColor: colors.yellow,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
           <div class="title">
             <TypistLoop interval={100}>
               {["frequently asked questions", ""].map((text) => (
@@ -30,12 +38,304 @@ export default class FaqComponent extends React.Component {
             style={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "center",
-              height: "100%",
+              backgroundColor: "white",
+              padding: "30px",
             }}
           >
-            {" "}
-            faq q's
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                1
+              </div>
+              <div
+                style={{
+                  color: colors.cherry,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<q>"}
+              </div>
+              <div
+                style={{ color: colors.black, fontWeight: fonts.weights.bold }}
+              >
+                I would love to work with Catalyst. How can I reach out?
+              </div>
+              <div
+                style={{ color: colors.cherry, fontWeight: fonts.weights.bold }}
+              >
+                {"</q>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                2
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<a>"}
+              </div>
+              <div
+                style={{
+                  color: colors.black,
+                  fontWeight: fonts.weights.regular,
+                }}
+              >
+                Please email us! We're always looking to collaborate with
+                different folks around campus.
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"</a>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                3
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                4
+              </div>
+              <div
+                style={{
+                  color: colors.cherry,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<q>"}
+              </div>
+              <div
+                style={{ color: colors.black, fontWeight: fonts.weights.bold }}
+              >
+                How can I join?
+              </div>
+              <div
+                style={{ color: colors.cherry, fontWeight: fonts.weights.bold }}
+              >
+                {"</q>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                5
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<a>"}
+              </div>
+              <div
+                style={{
+                  color: colors.black,
+                  fontWeight: fonts.weights.regular,
+                }}
+              >
+                Lucky you! We hold rush each fall and spring, but the upcoming
+                semester may be different.
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                6
+              </div>
+              <div
+                style={{
+                  color: colors.black,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.regular,
+                }}
+              >
+                Join our Facebook page for more information.
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"</a>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                7
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                8
+              </div>
+              <div
+                style={{
+                  color: colors.cherry,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<q>"}
+              </div>
+              <div
+                style={{ color: colors.black, fontWeight: fonts.weights.bold }}
+              >
+                Are your events open to everyone?
+              </div>
+              <div
+                style={{ color: colors.cherry, fontWeight: fonts.weights.bold }}
+              >
+                {"</q>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                9
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  marginLeft: "24px",
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"<a>"}
+              </div>
+              <div
+                style={{
+                  color: colors.black,
+                  fontWeight: fonts.weights.regular,
+                }}
+              >
+                Some of our events are open to everyone at Duke, and we would
+                love to have you there.
+              </div>
+              <div
+                style={{
+                  color: colors.catablue,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                {"</a>"}
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div
+                style={{
+                  color: colors.lightGray,
+                  fontWeight: fonts.weights.bold,
+                }}
+              >
+                10
+              </div>
+            </div>
           </div>
         </div>
       </div>
